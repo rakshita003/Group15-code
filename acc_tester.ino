a8cb1ef9a64589fe01c2a273161bfa938e258216
rdegans <38084642+rdegans@users.noreply.github.com>
2021-03-04 16:23:12 -0500

    Add files via upload

---
+++ acc_tester.ino
@@ -0,0 +1,94 @@
+#include <SparkFunLSM6DS3.h>
+
+/******************************************************************************
+MinimalistExample.ino
+
+Marshall Taylor @ SparkFun Electronics
+May 20, 2015
+https://github.com/sparkfun/LSM6DS3_Breakout
+https://github.com/sparkfun/SparkFun_LSM6DS3_Arduino_Library
+
+Description:
+Most basic example of use.
+
+Example using the LSM6DS3 with basic settings.  This sketch collects Gyro and
+Accelerometer data every second, then presents it on the serial monitor.
+
+Resources:
+Uses Wire.h for i2c operation
+
+Development environment specifics:
+Arduino IDE 1.6.4
+Teensy loader 1.23
+
+Hardware connections:
+Connect I2C SDA line to A4
+Connect I2C SCL line to A5
+Connect GND and 3.3v power to the IMU
+
+This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+
+Please review the LICENSE.md file included with this example. If you have any questions
+or concerns with licensing, please contact techsupport@sparkfun.com.
+
+Distributed as-is; no warranty is given.
+******************************************************************************/
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_I2CRegister.h>
+#include <Adafruit_SPIDevice.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ISM330DHCX.h>
+#include <Adafruit_LSM6DS.h>
+#include <Adafruit_LSM6DS3.h>
+#include <Adafruit_LSM6DS33.h>
+#include <Adafruit_LSM6DSL.h>
+#include <Adafruit_LSM6DSO32.h>
+#include <Adafruit_LSM6DSOX.h>
+#include <Arduino_LSM6DS3.h>
+#include <SparkFunLSM6DS3.h>
+#include "Wire.h"
+#include "SPI.h"
+
+LSM6DS3 myIMU( SPI_MODE, 10 );
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(1000); //relax...
+  Serial.println("Processor came out of reset.\n");
+  Wire.begin();
+  SPI.begin();
+  //Call .begin() to configure the IMU
+  myIMU.begin();
+ 
+}
+a
+
+void loop()
+{
+  //Get all parameters
+  Serial.print("\nAccelerometer:\n");
+  Serial.print(" X = ");
+  Serial.println(myIMU.readFloatAccelX(), 4);
+  Serial.print(" Y = ");
+  Serial.println(myIMU.readFloatAccelY(), 4);
+  Serial.print(" Z = ");
+  Serial.println(myIMU.readFloatAccelZ(), 4);
+
+  Serial.print("\nGyroscope:\n");
+  Serial.print(" X = ");
+  Serial.println(myIMU.readFloatGyroX(), 4);
+  Serial.print(" Y = ");
+  Serial.println(myIMU.readFloatGyroY(), 4);
+  Serial.print(" Z = ");
+  Serial.println(myIMU.readFloatGyroZ(), 4);
+
+  Serial.print("\nThermometer:\n");
+  Serial.print(" Degrees C = ");
+  Serial.println(myIMU.readTempC(), 4);
+  Serial.print(" Degrees F = ");
+  Serial.println(myIMU.readTempF(), 4);
+ 
+  delay(1000);
+}
